1a59d6ec3ce20ba6f64cc12bcb8c610e21ab7190
Thomas Ringling <thomas.ringling@gmail.com>
2018-06-22 19:44:22 +0200

    Sponsors 2018

--- pages/index.vue
+++ pages/index.vue
@@ -10,74 +10,20 @@
           <v-flex d-flex>
             <v-layout row wrap class="event-card">
 
-
               <v-flex d-flex xs12>
                 <v-card>
                   <v-toolbar color="blue-grey lighten-5" flat>
-                    <v-toolbar-title>Pre- & post programs</v-toolbar-title>
-                  </v-toolbar>
-                  <v-card-text>
-                    <v-container fluid grid-list-md>
-
-
-
-
-
-                      <v-layout row wrap class="text-xs-center">
-                        <v-flex xs6>
-                          <b>Sightseeing in Copenhagen</b><br>
-                          Pre-program<br>
-                          <a href="https://www.linkedin.com/pulse/accdk-2018-pre-program-sightseeing-copenhagen-karen-trolle-schutter/" target="_blank">Read more on LinkedIn</a>
-
-                        </v-flex>
-                        <v-flex xs6 >
-                          <b>Beer Festival</b><br>
-                          Post-program<br>
-                          <a href="https://www.linkedin.com/pulse/accdk-2018-post-program-beer-festival-karen-trolle-schutter/" target="_blank">Read more on LinkedIn</a>
-
-
-
-                        </v-flex>
-                      </v-layout>
-                    </v-container>
-                  </v-card-text>
-                </v-card>
-              </v-flex>
-               <v-flex d-flex xs12>
-                <v-card>
-                  <v-toolbar color="blue-grey lighten-5" flat>
-                    <v-toolbar-title>Masterclass</v-toolbar-title>
-                  </v-toolbar>
-                  <v-card-text>
-                    <v-container fluid grid-list-md>
-                      <v-layout row wrap class="text-xs-center">
-                        <v-flex xs12 >
-                          With <b>Karen Tobiasen</b>, Chief People Officer and Head of People at Nordea, <b>“What’s love got to do with it”</b>.
-                          <br><a href="https://www.linkedin.com/pulse/accdk-whats-love-got-do-karen-trolle-schutter/" target="_blank">Read more on LinkedIn</a>
-                        </v-flex>
-                      </v-layout>
-                    </v-container>
-                  </v-card-text>
-                </v-card>
-              </v-flex>
-
-
-
-
-
-
-              <v-flex d-flex xs12>
-                <v-card>
-                  <v-toolbar dark color="orange" flat>
-                    <v-toolbar-title>Sale opened for ACCDK 2018</v-toolbar-title>
+                    <v-toolbar-title>ACCDK 2019 on May 23-25
+                    </v-toolbar-title>
                   </v-toolbar>
                   <v-card-text>
 
                     <v-container fluid grid-list-md>
                       <v-layout row wrap class="text-xs-center">
                         <v-flex xs6>
-                          <div class="headline">May 31 - June 2</div>
-                          <div class="subheading">2018</div>
+                          <div class="subheading">Sales opens on</div>
+                          <div class="headline">January 31</div>
+                          <div class="subheading">2019</div>
                         </v-flex>
                         <v-flex xs6>
                           <b>Dragør Badehotel</b><br>
@@ -90,13 +36,7 @@
                   </v-card-text>
                   <v-card-actions>
                     <v-layout row wrap class="text-xs-center">
-                      <v-flex xs6>
-                        <v-btn flat color="orange" value="recent" target="_blank" href="https://www.eventbrite.com/e/accdk2018-tickets-32622618129">
-                          <v-icon>receipt</v-icon>
-                          <span>Buy Tickets</span>
-                        </v-btn>
-                      </v-flex>
-                      <v-flex xs6>
+                      <v-flex xs12>
                         <v-btn flat color="indigo lighten-2" value="nearby" target="_blank" href="https://www.google.com/maps/place/Copenhagen+Airporthotel+%2F+Drag%C3%B8r+Badehotel/@55.5902562,12.6710978,17z/data=!3m1!4b1!4m5!3m4!1s0x4653aa4b12df335d:0x55a344639cdb1934!8m2!3d55.5902562!4d12.6732865">
                           <v-icon>place</v-icon>
                           <span>Google Maps</span>
@@ -108,8 +48,6 @@
               </v-flex>
 
 
-
-
               <v-flex d-flex xs12>
                 <v-card>
                   <v-toolbar color="blue-grey lighten-5" flat>
@@ -118,16 +56,39 @@
                   <v-card-text>
                     <v-container fluid grid-list-md>
                       <v-layout row wrap class="text-xs-center">
-                        <v-flex xs12 >
+                        <v-flex xs6 >
                           <img class="sponsor-logo-tiny" src="goagile-tiny.jpg"></img>
-                          <div class="headline orange--text">There is room for more sponsors</div>
+                        </v-flex>
+                        <v-flex xs6 >
+                          <img class="sponsor-logo-tiny" style="max-width: 150px" src="zenit.png"></img>
+                        </v-flex>
+                        <v-flex xs6 >
+                          <img class="sponsor-logo-tiny" src="ugilic.png"></img>
+                        </v-flex>
+                        <v-flex xs6 >
+                          <img class="sponsor-logo-tiny" src="kmd.png"></img>
                         </v-flex>
                       </v-layout>
                     </v-container>
                   </v-card-text>
                 </v-card>
               </v-flex>
-
+              <v-flex d-flex xs12>
+                <v-card>
+                  <v-toolbar color="blue-grey lighten-5" flat>
+                    <v-toolbar-title>2019 sponsors</v-toolbar-title>
+                  </v-toolbar>
+                  <v-card-text>
+                    <v-container fluid grid-list-md>
+                      <v-layout row wrap class="text-xs-center">
+                        <v-flex xs12 >
+                          <div class="headline orange--text">There is room for sponsors</div>
+                        </v-flex>
+                      </v-layout>
+                    </v-container>
+                  </v-card-text>
+                </v-card>
+              </v-flex>
             </v-layout>
           </v-flex>
         </v-layout>
@@ -137,16 +98,16 @@
       <v-flex d-flex xs12 sm6 md8>
         <v-card>
           <v-toolbar color="blue-grey lighten-5" flat>
-            <v-toolbar-title>Greetings from ACCDK 2017</v-toolbar-title>
+            <v-toolbar-title>Greetings from ACCDK 2018</v-toolbar-title>
           </v-toolbar>
-          <v-card-media src="accdk_2017_end.jpg" height="400px"></v-card-media>
+          <v-card-media src="accdk_2018_end.jpg" height="550px"></v-card-media>
           <v-card-title primary-title>
             <v-layout row wrap class="text-xs-center">
               <v-flex xs6>
-                <div class="subheading">Held on May 18 - May 20</div>
+                <div class="subheading">Held on May 31 - June 2</div>
               </v-flex>
               <v-flex xs6>
-                <a target="_blank" href="https://twitter.com/hashtag/accdk17">2017 ACCDK Tweets</a>
+                <a target="_blank" href="https://twitter.com/hashtag/accdk18">2018 ACCDK Tweets</a>
               </v-flex>
             </v-layout>
           </v-card-title>
